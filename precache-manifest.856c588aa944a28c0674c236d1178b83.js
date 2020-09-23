cd0e99a4510fa1f9b3b51a43947a17572031cbad
Nicole Graiff <nicole.graiff@gmail.com>
2020-09-22 20:33:58 -0400

    Updates

---
+++ precache-manifest.856c588aa944a28c0674c236d1178b83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb4811750ed7a6d5ecbf94458899fddb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca0980addca4610d0446",
+    "url": "/static/css/main.7b211e5f.chunk.css"
+  },
+  {
+    "revision": "4479320bf5aa5c1d9101",
+    "url": "/static/js/2.7acf15c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7acf15c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca0980addca4610d0446",
+    "url": "/static/js/main.c4b8d93d.chunk.js"
+  },
+  {
+    "revision": "7a235cb05d8669b2d22d",
+    "url": "/static/js/runtime-main.7ba75982.js"
+  }
+]);
